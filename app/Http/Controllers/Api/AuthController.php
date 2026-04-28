4b6127c25ab4a8164ec42c7986cbfca7c544f90f
Blaž Oražem <blaz.orazem@spletna-postaja.com>
2026-04-28 22:26:51 +0200

    Update password policy

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
         $data = $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', 'unique:users,email'],
-            'password' => ['required', 'string', Password::min(8)->mixedCase()->numbers()],
+            'password' => ['required', 'string', Password::min(8)],
         ]);
 
         $user = User::create($data);
